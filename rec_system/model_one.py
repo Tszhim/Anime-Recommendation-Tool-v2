5fc9d2878b9abc24d2925ebf345f1a66872dbbcf
allen-siu <allenlsiu@gmail.com>
2022-08-11 23:26:24 -0400

    created dataframes to work with

--- rec_system/model_one.py
+++ rec_system/model_one.py
@@ -8,12 +8,17 @@ engine, conn, meta_data = get_db_connection()
 full_table = get_full_merge(conn, meta_data)
 
 # Set pandas options for printing
-pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', 10)
 pd.set_option('display.width', None)
 
 # Convert table to DataFrame
 full_table = pd.DataFrame(full_table)
 
+# Create separate DataFrame containing only USERNAME, ANIME_TITLE, USER_RATING
+user_item_table = full_table.filter(['USERNAME', 'SCORE', 'ANIME_TITLE'], axis=1)
 
-
-print(full_table.head())
+# Create DataFrame where USERNAME is rows, ANIME_TITLE is columns, USER_RATING is entry [USERNAME, ANIME_TITLE]
+user_item_table = user_item_table.pivot_table(index=['USERNAME'],
+                                              columns='ANIME_TITLE',
+                                              values='SCORE')
+print(user_item_table.head())
